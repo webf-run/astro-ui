b63600945ddd9af116467a2bff48a77172bcca64
Jayshri Kekan <jayshri.kekan@tekbuds.com>
2026-07-03 20:28:46 +0530

    initial setup of doc-site, created navbar and homepage for website, created button doc component for doc site

---
+++ ui/astro.config.mjs
@@ -0,0 +1,48 @@
+import starlight from '@astrojs/starlight';
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: '@webf/astro-ui',
+      social: [
+        {
+          icon: 'github',
+          label: 'GitHub',
+          href: 'https://github.com/webf-run/astro-ui',
+        },
+      ],
+      components: {
+        Header: './src/components/starlight/Header.astro',
+      },
+
+      customCss: ['./src/styles/custom.css'],
+      sidebar: [
+        {
+          label: 'Getting Started',
+          items: [
+            {
+              label: 'Installation',
+              slug: 'getting-started/installation',
+            },
+          ],
+        },
+        {
+          label: 'Components',
+          items: [
+            {
+              label: 'Button',
+              slug: 'components/button',
+            },
+          ],
+        },
+      ],
+    }),
+  ],
+
+vite: {
+  ssr: {
+    noExternal: ['lucide-astro'],
+  },
+},
+});
