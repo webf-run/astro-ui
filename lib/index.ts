2c14cb922ff103e2aaf8151c5debd4612cc5b83e
Jayshri Kekan <jayshri.kekan@tekbuds.com>
2026-07-29 19:37:29 +0530

    Added new ui components badge, logo card, info card,stat card

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,46 @@
 export { default as Navbar } from './components/Navbar.astro';
 export { default as Button } from './components/Button.astro';
-export { default as SectionBadge } from './components/SectionBadge.astro';
+export { default as Badge } from './components/Badge.astro';
 export { default as StatCard } from './components/StatCard.astro';
-export { default as FeatureCard } from './components/FeatureCard.astro';
-export { default as CompanyLogoCard } from './components/CompanyLogoCard.astro';
+export { default as InfoCard } from './components/InfoCard.astro';
+export { default as LogoCard } from './components/LogoCard.astro';
 export { default as PlacementStatCard } from './components/PlacementStatCard.astro';
+export {
+  Users,
+  GraduationCap,
+  DollarSign,
+  FolderKanban,
+  Bell,
+  HeartHandshake,
+  Briefcase,
+  Building2,
+  Award,
+  ShoppingBag,
+  Globe,
+  Sparkles,
+  Headphones,
+  Download,
+  Star,
+  Smile,
+  Zap,
+  ShieldCheck,
+  PlugZap,
+  Server,
+  Stethoscope,
+  HeartPulse,
+  ShoppingCart,
+  Package,
+  Box,
+  MapPinned,
+  BarChart3,
+  TrendingUp,
+  TrendingDown,
+  CreditCard,
+  Eye,
+  Percent,
+  Wallet,
+  Clock,
+  Truck,
+  Heart,
+  MessageCircle,
+} from 'lucide-astro';
